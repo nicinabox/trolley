38eb4c2ccf3a8c4dc3e3debcbd6e08b3596b83ee
Nic Aitch <nic@nicinabox.com>
2013-10-01 15:45:21 -0500

    Check for python, install 2.7.3 if doesn't exist

--- install/doinst.sh
+++ install/doinst.sh
@@ -3,3 +3,9 @@
 if [[ ! `command -v curl` ]]; then
   wget -qO- http://boxcar.nicinabox.com/install_curl | sh -
 fi
+
+if [[ ! `command -v python` ]]; then
+  wget http://slackware.cs.utah.edu/pub/slackware/slackware-14.0/slackware/d/python-2.7.3-i486-2.txz
+  installpkg python-2.7.3-i486-2.txz
+  mv python-2.7.3-i486-2.txz /boot/extra
+fi
