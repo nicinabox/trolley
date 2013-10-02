5606d3adcae913889ed44b2b642044b36b8939de
Nic Aitch <nic@nicinabox.com>
2013-10-02 16:21:09 -0500

    Install setuptools and argparse in doinst.sh

--- install/doinst.sh
+++ install/doinst.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Install curl, if needed
 if [[ ! `command -v curl` ]]; then
   wget -qO- http://boxcar.nicinabox.com/install_curl | sh -
 fi
 
+# Install python 2.6.6, if needed
 if [[ ! `command -v python` ]]; then
-  wget http://slackware.cs.utah.edu/pub/slackware/slackware-14.0/slackware/d/python-2.7.3-i486-2.txz
-  installpkg python-2.7.3-i486-2.txz
-  mv python-2.7.3-i486-2.txz /boot/extra
+  python_url=http://slackware.cs.utah.edu/pub/slackware/slackware-13.37/slackware/d/python-2.6.6-i486-1.txz
+  name=`basename $python_url`
+  wget $python_url
+  installpkg $name
+  mv $name /boot/extra
 fi
+
+# Install setuptools
+setuptools=https://pypi.python.org/packages/source/s/setuptools/setuptools-1.1.6.tar.gz
+name=`basename $setuptools`
+wget --no-check-certificate $setuptools
+tar xzf $name && cd ${name%.*.*}
+python setup.py install
+
+# Install argparse
+easy_install argparse
