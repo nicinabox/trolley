ded2f4ca46520491718f001f05da583eae2d3955
Nic Aitch <nic@nicinabox.com>
2013-10-04 13:11:43 -0500

    Add installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+version=0.1.3
+
+wget -q --no-check-certificate \
+     -O /boot/extra/trolley-$version-noarch-unraid.txz \
+     https://github.com/nicinabox/trolley/releases/download/$version/trolley-$version-noarch-unraid.txz
+
+installpkg /boot/extra/trolley-$version-noarch-unraid.txz
