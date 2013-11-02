261a69a6702014ce1148ad16e24bb39895bf45b9
Nic Aitch <nic@nicinabox.com>
2013-11-01 21:57:00 -0500

    Update updater

--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 version=0.1.5
 
+# Remove old file versions
+rm -rf /boot/extra/trolley*
+
+# Download new
 wget -q --no-check-certificate \
      -O /boot/extra/trolley-$version-noarch-unraid.tgz \
      https://github.com/nicinabox/trolley/releases/download/$version/trolley-$version-noarch-unraid.tgz
 
+# Install
 installpkg /boot/extra/trolley-$version-noarch-unraid.tgz
