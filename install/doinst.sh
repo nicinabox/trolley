5dfdc602aa7aeb553a809faa8acdc6dc5d233e7c
Nic Aitch <nic@nicinabox.com>
2014-01-21 22:55:57 -0600

    Refactor post installer

--- install/doinst.sh
+++ install/doinst.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 source /usr/local/boiler/trolley/env
+source /etc/bundlerc
+
 PATH=$TROLLEY_LIB_PATH/setup:$PATH
 
-# Install package dependencies
-fetchpkg -p /patches/packages/linux-2.6.37.6-3/kernel-headers-2.6.37.6_smp-x86-3.txz -s 13.37 -i
-fetchpkg -p /patches/packages/glibc-2.15-i486-8_slack14.0.txz -s 14.0 -i
-fetchpkg -p /slackware/n/curl-7.20.1-i486-1.txz -s 13.1 -i
-fetchpkg -p /slackware/l/libyaml-0.1.4-i486-1.txz -s 14.1 -i
-fetchpkg -p /slackware/n/openssl-1.0.1c-i486-3.txz -s 14.0 -i
-fetchpkg -p /slackware/d/automake-1.11.5-noarch-1.txz -s 14.1 -i
-fetchpkg -p /slackware/l/libmpc-0.8.2-i486-2.txz -s 14.1 -i
-fetchpkg -p /slackware/l/libelf-0.8.13-i486-2.txz -s 14.1 -i
-fetchpkg -p /slackware/l/mpfr-3.0.1-i486-1.txz -s 13.37 -i
-fetchpkg -p /slackware/d/gcc-4.5.2-i486-2.txz -s 13.37 -i
-fetchpkg -p /slackware/d/gcc-g++-4.5.2-i486-2.txz -s 13.37 -i
-fetchpkg -p /slackware/d/binutils-2.21.51.0.6-i486-1.txz -s 13.37 -i
-fetchpkg -p /slackware/d/automake-1.11.5-noarch-1.txz -s 14.1 -i
-fetchpkg -p /slackware/d/make-3.82-i486-4.txz -s 14.1 -i
-fetchpkg -p /slackware/l/zlib-1.2.8-i486-1.txz -s 14.1 -i
+if ! grep -Fxq "source /etc/bundlerc" /etc/profile; then
+  echo 'source /etc/bundlerc' >> /etc/profile
+fi
 
-fetchpkg -p /slackware/d/git-1.8.4-i486-1.txz -s 14.1 -i
-fetchpkg -p /patches/packages/ruby-1.9.3_p484-i486-1_slack14.1.txz -s 14.1 -i
+# Bootstrap package dependencies
+fetchpkg -p /slackware/d/python-2.6.6-i486-1.txz -s 13.37 -i
 
+plz.py install kernel-headers 2.6.37.6_smp
+plz.py install binutils 2.21.51.0.6
+plz.py install glibc 2.15
+plz.py install gcc 4.5.2
+plz.py install gcc-g 4.5.2  # gcc-g++
+plz.py install libmpc 0.8.2
+plz.py install libelf 0.8.13
+plz.py install openssl 1.0.1c
+plz.py install automake 1.11.5
+plz.py install make 3.82
+plz.py install curl 7.20.1
+plz.py install libyaml 0.1.4
+plz.py install mpfr 3.0.1
+plz.py install zlib 1.2.8
+plz.py install git 1.8.4
+plz.py install ruby 1.9.3_p484
+
+if [[ `gem -v` < "2.2" ]]; then
+  gem update --system
+fi
+
+if [[ `command -v bundle` == "" ]]; then
+  gem install bundler
+fi
